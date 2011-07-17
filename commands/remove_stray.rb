94c571f4174dbb27fb9143450ed01a94076256b5
Denis Defreyne <denis.defreyne@stoneship.org>
2011-07-17 11:32:05 +0200

    renamed clean_all task to remove_stray command
    --HG--
    rename : tasks/clean.rake => commands/remove_stray.rb

--- commands/remove_stray.rb
+++ commands/remove_stray.rb
@@ -1,10 +1,16 @@
 # encoding: utf-8
 
-desc 'Removes all stray files from the output directory'
-task :clean_all do
+usage       'remove_stray'
+summary     'remove stray files from output directory'
+description <<-EOS
+Removes all stray files, i.e. all files not managed by nanoc, from the output directory. This is useful if you also remove remote files when deploying (e.g. with rsync’s --delete-after).
+
+This command should be safe to run if you do not put any files in the output directory manually.
+EOS
+
+run do |opts, args, cmd|
   # Get compiled files
   site = Nanoc3::Site.new('.')
-  site.load_data
   compiled_files = site.items.map do |item|
     item.reps.map do |rep|
       rep.raw_path
